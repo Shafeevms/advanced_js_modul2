69fc53f812cc3346d41b300b0c27463f78a77584
Max Shafeev <shafeevms@gmail.com>
2021-06-30 15:27:58 +0300

    refactoring

--- scripts/store.js
+++ scripts/store.js
@@ -4,30 +4,24 @@ export const store = {
   store: null,
 };
 
+const server = document.querySelector('#server');
+
 export const getItems = async () => {
-  const storage = document.querySelector('#server').checked
-  ? await import ('./server.js')
-  : await import ('./local.js');
+  const storage = await import (server.checked ?'./server.js' : './local.js');
   await storage.getData();
 };
 
 export const setItems = async (name) => {
-  const storage = document.querySelector('#server').checked
-  ? await import ('./server.js')
-  : await import ('./local.js');
+  const storage = await import (server.checked ?'./server.js' : './local.js');
   storage.setData(name);
 };
 
 export const onDone = async (e) => {
-  const storage = document.querySelector('#server').checked
-  ? await import ('./server.js')
-  : await import ('./local.js');
+  const storage = await import (server.checked ?'./server.js' : './local.js');
   storage.done(e);
 };
 
 export const onDelete = async (e) => {
-  const storage = document.querySelector('#server').checked
-  ? await import ('./server.js')
-  : await import ('./local.js');
+  const storage = await import (server.checked ?'./server.js' : './local.js');
   storage.del(e);
 };
